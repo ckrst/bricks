13922799c8df1099f59d36759c4fd3713b89bfbc
Vinícius Kirst <vinicius.kirst@gmail.com>
2014-05-21 08:43:57 -0300

    desversionando db e cache

--- php/app/Config/database.php
+++ php/app/Config/database.php
@@ -1,10 +1,10 @@
 <?php
 
-define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT'));
-define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
+define('DB_HOST', 'localhost');
+define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'coisa1');
 
 /**
  *
