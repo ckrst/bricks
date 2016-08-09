a906e3869bfba72bc65880ef671a25460a66cabd
vinik <vinicius.kirst@gmail.com>
2016-08-08 21:06:03 -0300

    another test attempt

--- php/lib/Cake/TestSuite/CakeTestSuiteDispatcher.php
+++ php/lib/Cake/TestSuite/CakeTestSuiteDispatcher.php
@@ -143,8 +143,8 @@ class CakeTestSuiteDispatcher {
 				break;
 			}
 		}
-		//include 'PHPUnit' . DS . 'Autoload.php';
-		include 'php/vendor/phpunit/phpunit/PHPUnit' . DS . 'Autoload.php';
+		include 'PHPUnit' . DS . 'Autoload.php';
+		# include 'php/vendor/phpunit/phpunit/PHPUnit' . DS . 'Autoload.php';
 		return class_exists('PHPUnit_Framework_TestCase');
 	}
 
